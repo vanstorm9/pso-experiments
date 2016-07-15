da8d4e47b9d2860848df00ba9ff7669067ff76ea
vanstorm9 <antlowhur@yahoo.com>
2016-07-15 11:06:39 -0500

    Managed to get particle to produce sine and cosine waves

--- simulations/animation-ball.py
+++ simulations/animation-ball.py
@@ -6,32 +6,47 @@ fig = plt.figure()
 fig.set_dpi(100)
 fig.set_size_inches(5, 4.5)
 
-ax = plt.axes(xlim=(0, 10), ylim=(0, 10))
-patch = plt.Circle((5, -5), 0.75, fc='y')
+ax = plt.axes(xlim=(0, 100), ylim=(0, 100))
+patch = plt.Circle((10, -10), 0.75, fc='r')
 
 def init():
     patch.center = (5, 5)
     ax.add_patch(patch)
     return patch,
 
-def animateCircTraj(i):
-    # i seems to go all the way from 1 to 360
-    # Affects the trajectory of the path
+def animateCirc(i):
+    # It seems that i represents time step
     x, y = patch.center
-    x = 5 + 3 * np.sin(np.radians(i))
-    y = 5 + 3 * np.cos(np.radians(i))
+    # 1st constant = position and 2nd constant = trajectory
+    x = 50 + 30 * np.sin(np.radians(i))
+    y = 50 + 30 * np.cos(np.radians(i))
     patch.center = (x, y)
     return patch,
+
+def animateLine(i):
+    x, y = patch.center
+    x = x + 1
+    y = x+ 1
+    patch.center = (x, y)
+    return patch,
+
 
 def animateCos(i):
     x, y = patch.center
-    x = i
-    y = np.cos(np.radians(i))
+    x = x + 0.2
+    y = 50 + 30 * np.cos(np.radians(i))
+    patch.center = (x, y)
+    return patch,
+
+def animateSin(i):
+    x, y = patch.center
+    x = x + 0.2
+    y = 50 + 30 * np.sin(np.radians(i))
     patch.center = (x, y)
     return patch,
 
 
-anim = animation.FuncAnimation(fig, animateCircTraj, 
+anim = animation.FuncAnimation(fig, animateCos, 
                                init_func=init, 
                                frames=360, 
                                interval=20,
